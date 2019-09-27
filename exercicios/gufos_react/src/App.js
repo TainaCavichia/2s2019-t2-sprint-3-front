16fda48bb47ee02536708a14f2fe63e6518cffc4
Tainá Cavichia Alves <46748080819@info.local>
2019-09-27 11:28:47 -0300

    sadsadfda

---
+++ exercicios/gufos_react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário</h1>
+      <form>
+        <input></input>
+      </form>
+      <button type="submit">Enviar</button>
+    </div>
+  );
+}
+
+export default App;
